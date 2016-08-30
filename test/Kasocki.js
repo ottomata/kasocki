7be5134dbe9187d1d8980f2d412877843e27d55a
Andrew Otto <acotto@gmail.com>
2016-08-30 10:43:01 -0400

    Save list of available topics on initialization and send this to client on ready

--- test/Kasocki.js
+++ test/Kasocki.js
@@ -4,12 +4,16 @@
 // mocha defines to avoid JSHint breakage
 /* global describe, it, before, beforeEach, after, afterEach */
 
-const assert = require('assert');
+//  NOTE: these tests require a running kafka broker at localhost:9092
 
-const http =  require('http');
-const socket_io = require('socket.io');
-const Kasocki = require('../lib/Kasocki');
-const bunyan      = require('bunyan');
+const assert       = require('assert');
+
+const http         = require('http');
+const socket_io    = require('socket.io');
+const Kasocki      = require('../lib/Kasocki');
+const bunyan       = require('bunyan');
+const P = require('bluebird');
+
 
 /**
  * Kasocki socket.io test server.
@@ -71,13 +75,18 @@ class TestKasockiServer {
 describe('Kasocki', function() {
     // this.timeout(5000);
 
+    const topicNames = [
+        'kasocki_test_01',
+        'kasocki_test_02'
+    ];
+
     var serverPort            = 6900;
     const server              = new TestKasockiServer(serverPort);
     var restrictiveServerPort = 6901;
-    const restrictiveServer   = new TestKasockiServer(restrictiveServerPort, {}, ['topic1', 'topic2']);
+    const restrictiveServer   = new TestKasockiServer(restrictiveServerPort, {}, topicNames);
 
     function createClient(port) {
-        return require('socket.io-client')(`http://localhost:${port}/`);
+        return P.promisifyAll(require('socket.io-client')(`http://localhost:${port}/`));
     }
 
 
@@ -91,10 +100,14 @@ describe('Kasocki', function() {
         restrictiveServer.close();
     });
 
+
+
+    // == Test subscribe ==
+
     it('should subscribe to a single topic', function(done) {
         const client = createClient(serverPort);
         client.on('ready', () => {
-            client.emit('subscribe', ['topic1'], (err, res) => {
+            client.emit('subscribe', [topicNames[0]], (err, res) => {
                 if (err)
                     throw err;
                 else
@@ -107,7 +120,7 @@ describe('Kasocki', function() {
     it('should subscribe to multiple topics', function(done) {
         const client = createClient(serverPort);
         client.on('ready', () => {
-            client.emit('subscribe', ['topic1', 'topic2'], (err, res) => {
+            client.emit('subscribe', topicNames, (err, res) => {
                 if (err)
                     throw err;
                 else
@@ -120,7 +133,7 @@ describe('Kasocki', function() {
     it('should fail subscribe to a single non existent topic', function(done) {
         const client = createClient(serverPort);
         client.on('ready', () => {
-            client.emit('subscribe', ['topic0'], (err, res) => {
+            client.emit('subscribe', ['non-existent-topic'], (err, res) => {
                 // TODO check err type?
                 if (err) {
                     done();
@@ -133,7 +146,7 @@ describe('Kasocki', function() {
     it('should fail subscribe to multiple topics, one of which does not exist', function(done) {
         const client = createClient(serverPort);
         client.on('ready', () => {
-            client.emit('subscribe', ['topic1', 'topic0'], (err, res) => {
+            client.emit('subscribe', [topicNames[0], 'non-existent-topic'], (err, res) => {
                 // TODO check err type?
                 if (err) {
                     done();
@@ -146,7 +159,7 @@ describe('Kasocki', function() {
     it('should subscribe to a single allowed topic', function(done) {
         const client = createClient(restrictiveServerPort);
         client.on('ready', () => {
-            client.emit('subscribe', ['topic1'], (err, res) => {
+            client.emit('subscribe', [topicNames[0]], (err, res) => {
                 if (err)
                     throw err;
                 else
@@ -159,7 +172,7 @@ describe('Kasocki', function() {
     it('should subscribe to a multiple allowed topics', function(done) {
         const client = createClient(restrictiveServerPort);
         client.on('ready', () => {
-            client.emit('subscribe', ['topic1', 'topic2'], (err, res) => {
+            client.emit('subscribe', topicNames, (err, res) => {
                 if (err)
                     throw err;
                 else
@@ -172,7 +185,7 @@ describe('Kasocki', function() {
     it('should fail subscribe to a single unallowed topic', function(done) {
         const client = createClient(restrictiveServerPort);
         client.on('ready', () => {
-            client.emit('subscribe', ['topic3'], (err, res) => {
+            client.emit('subscribe', ['non-existent-topic'], (err, res) => {
                 // TODO check err type?
                 if (err) {
                     done();
@@ -185,7 +198,7 @@ describe('Kasocki', function() {
     it('should fail subscribe to a multiple topics with at least one not allowed', function(done) {
         const client = createClient(restrictiveServerPort);
         client.on('ready', () => {
-            client.emit('subscribe', ['topic1', 'not_allowed'], (err, res) => {
+            client.emit('subscribe', [topicNames[0], 'non-existent-topic'], (err, res) => {
                 if (err) {
                     done();
                 }
@@ -193,5 +206,20 @@ describe('Kasocki', function() {
             });
         });
     });
+
+
+    // == Test consume ==
+
+    // it('should consume a single message from a single topic'), function(done) {
+    //     const client = createClient(serverPort);
+    //     client.on('ready', () => {
+    //         client.emitAsync('subscribe', [topicNames[0]])
+    //         .then((res) => {
+    //             console.log(res);
+    //             done();
+    //             client.disconnect();
+    //         });
+    //     });
+    // }
 
 });
---
+++ test/utils/clean_kafka.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+KAFKA_TOPICS_CMD="kafka-topics"
+KAFKA_CONSOLE_PRODUCER_CMD="kafka-console-producer"
+
+
+dropTopics ( ) {
+  if [ "$#" -eq 1 ]
+  then
+    PATTERN=$1
+    echo "looking for topics named '*${PATTERN}*'..."
+    TOPICS=`${KAFKA_TOPICS_CMD} --zookeeper localhost:2181 --list \
+    	| grep ${PATTERN} \
+    	| grep -v 'marked for deletion$'`
+    for TOPIC in ${TOPICS}
+    do
+      echo "dropping topic ${TOPIC}"
+      ${KAFKA_TOPICS_CMD} --zookeeper localhost:2181 --delete --topic ${TOPIC} > /dev/null
+    done
+  fi
+}
+
+createTopic ( ) {
+    echo "Creating topic ${1}"
+    ${KAFKA_TOPICS_CMD} --create \
+        --zookeeper 127.0.0.1:2181             \
+        --partitions 1                         \
+        --replication-factor 1                 \
+        --topic $1 > /dev/null
+}
+
+produceTestData ( ) {
+    echo "Producing ${2} into topic ${1}"
+    ${KAFKA_CONSOLE_PRODUCER_CMD} --broker-list localhost:9092 --topic ${1} < ${2}
+}
+
+check ( ) {
+  PORT=$1
+  SERVICE_NAME=$2
+  if [ `nc localhost ${PORT} < /dev/null; echo $?` != 0 ]; then
+    echo "${SERVICE_NAME} not running, start it first"
+    exit 1
+  fi
+}
+
+check 2181 "Zookeeper"
+check 9092 "Kafka"
+dropTopics "kasocki_test_"
+sleep 5
+
+createTopic kasocki_test_01
+createTopic kasocki_test_02
+
+produceTestData kasocki_test_01 $(dirname $0)/test_data1.json
+produceTestData kasocki_test_02 $(dirname $0)/test_data2.json
